26be9926a9635a62eec5628fd6993458237911eb
Wang Shibiao <645102170@qq.com>
2016-06-24 23:09:15 +0800

    添加功能： 1. 利用mybatis工具生成org表相关类 2. 编写测试代码

--- springmvcHelloworld/src/test/java/com/wangshibiao/spring/dao/UserDaoTest.java
+++ springmvcHelloworld/src/test/java/com/wangshibiao/spring/dao/UserDaoTest.java
@@ -1,10 +1,12 @@
 package com.wangshibiao.spring.dao;
 
 import com.wangshibiao.spring.BaseTest;
+import com.wangshibiao.spring.model.Org;
 import com.wangshibiao.spring.model.User;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by Wang Shibiao on 2016/6/24.
@@ -12,10 +14,15 @@ import javax.annotation.Resource;
 public class UserDaoTest extends BaseTest{
     @Resource
     UserMapper userMapper;
+    @Resource
+    OrgMapper orgMapper;
 
     @Test
     public void test(){
         User user = userMapper.selectByPrimaryKey(27);
+        List orgList = orgMapper.selectByExample(null);
     }
+
+
 
 }

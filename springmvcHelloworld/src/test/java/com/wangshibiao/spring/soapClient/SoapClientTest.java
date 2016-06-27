693d0ca702139cf268b830f1a1f07e34143b9b65
Wang Shibiao <645102170@qq.com>
2016-06-27 16:49:33 +0800

    添加功能： 编写soap client，访问奥鹏oems的soap service

--- springmvcHelloworld/src/test/java/com/wangshibiao/spring/soapClient/SoapClientTest.java
+++ springmvcHelloworld/src/test/java/com/wangshibiao/spring/soapClient/SoapClientTest.java
@@ -1,6 +1,9 @@
 package com.wangshibiao.spring.soapClient;
 
 import com.wangshibiao.spring.BaseTest;
+import com.wangshibiao.spring.oemsSoapClient.OemsSmsService;
+import com.wangshibiao.spring.oemsSoapClient.OemsSmsServiceSoap;
+import com.wangshibiao.spring.oemsSoapClient.SmsSendSoapHeader;
 import org.junit.Test;
 
 import javax.xml.namespace.QName;
@@ -33,4 +36,28 @@ public class SoapClientTest extends BaseTest{
         Helloworld helloworld = helloworldImplService.getHelloworldImplPort();
         System.out.println(helloworld.method1("good"));
     }
+
+    /**
+     * 测试oems的soap service
+     * 通过wsimport生成的客户端代码访问soap服务,
+     * 生成客户端代码的命令：wsimport -XadditionalHeaders -s F:\github\springmvc\springmvcHelloworld\src\main\java -p com.wangshibiao.spring.oemsSoapClient http://116.213.120.55/OpenSMSWebService/OemsSmsService.asmx?wsdl
+     */
+    @Test
+    public void oems(){
+        String mobilephoneno = "15201592893";
+        String msgTitle  ="title1";
+        String msgContent = "content1";
+        Holder<Integer> smsSendResult = new Holder<Integer>();
+        Holder<String> smsReturnValue = new Holder<String>();
+        SmsSendSoapHeader smsSendSoapHeader = new SmsSendSoapHeader();
+        smsSendSoapHeader.setSendSource("oesspmcauth");
+        smsSendSoapHeader.setSendSourcePwd("oesspmcauth-auth-pass-word-2016-0622");
+
+        OemsSmsService oemsSmsService = new OemsSmsService();
+        OemsSmsServiceSoap oemsSmsServiceSoap = oemsSmsService.getPort(OemsSmsServiceSoap.class);
+        oemsSmsServiceSoap.smsSend(mobilephoneno, msgTitle, msgContent, smsSendResult, smsReturnValue, smsSendSoapHeader);
+        Integer sendResult = smsSendResult.value;
+        String returnValue = smsReturnValue.value;
+    }
+
 }

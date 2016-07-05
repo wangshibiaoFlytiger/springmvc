ca104c399d043229477c6e0413113db7696c2d4b
Wang Shibiao <645102170@qq.com>
2016-07-05 10:50:27 +0800

    添加功能：soap service类可依赖注入bean

--- springmvcHelloworld/src/main/java/com/wangshibiao/spring/soapService/Helloworld.java
+++ springmvcHelloworld/src/main/java/com/wangshibiao/spring/soapService/Helloworld.java
@@ -3,10 +3,12 @@ package com.wangshibiao.spring.soapService;
 import com.wangshibiao.spring.dao.UserMapper;
 import com.wangshibiao.spring.model.User;
 import com.wangshibiao.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import javax.annotation.Resource;
 import javax.jws.WebMethod;
@@ -20,12 +22,13 @@ import javax.jws.soap.SOAPBinding;
 
 /**
  * 生成webservice剩余代码，执行命令：wsgen -wsdl -r F:\github\springmvc\springmvcHelloworld\src\main\resources\soap -s F:\github\springmvc\springmvcHelloworld\src\main\java -classpath F:\github\springmvc\out\build\springmvcHelloworld\classes\main\ com.wangshibiao.spring.soapService.Helloworld
+ * 注意：执行wsgen之前先把extends SpringBeanAutowiringSupport注掉，执行完成后再恢复，否则命令提示错误
  */
-//@Service
+
 @WebService
-public class Helloworld {
-    /*@Resource
-    UserService userService;*/
+public class Helloworld extends SpringBeanAutowiringSupport{
+    @Autowired
+    UserService userService;
 
     @WebMethod
     public String method1(String name){
@@ -34,13 +37,7 @@ public class Helloworld {
 
     @WebMethod
     public User getUser(@WebParam(name = "username") String username){
-        /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("mainCtx.xml");
-        UserService userService = (UserService) applicationContext.getBean("userService");
-        User user = userService.getUserByUserName(username);*/
-        User user = new User();
-        user.setUsername("admin");
-        user.setPassword("pass");
-        //User user = userService.getUserByUserName("admin");
+        User user = userService.getUserByUserName(username);
         return user;
     }
 }

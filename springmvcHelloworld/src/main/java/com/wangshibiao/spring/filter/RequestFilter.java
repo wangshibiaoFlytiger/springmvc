0160f86badffb1c6a5140c285ce23c891c651843
Wang Shibiao <645102170@qq.com>
2016-12-20 11:14:25 +0800

    优化代码

--- springmvcHelloworld/src/main/java/com/wangshibiao/spring/filter/RequestFilter.java
+++ springmvcHelloworld/src/main/java/com/wangshibiao/spring/filter/RequestFilter.java
@@ -1,6 +1,5 @@
 package com.wangshibiao.spring.filter;
 
-import com.sun.org.glassfish.external.statistics.annotations.Reset;
 import com.wangshibiao.spring.model.User;
 import com.wangshibiao.spring.service.UserService;
 import org.slf4j.Logger;
@@ -31,9 +30,18 @@ public class RequestFilter extends SpringBeanAutowiringSupport implements Filter
 
     }
 
+    /**
+     * 请求进入controller之前进行过滤
+     * 经测试发现：如果请求地址没有通过shiro的认证或授权，过滤器收不到请求参数
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        logger.debug("进入RequestFilter doFilter");
+       logger.debug("进入RequestFilter doFilter");
         String token = servletRequest.getParameter("token");
         String username = servletRequest.getParameter("username");
         if (!StringUtils.isEmpty(username)){
@@ -42,10 +50,12 @@ public class RequestFilter extends SpringBeanAutowiringSupport implements Filter
                 System.out.println("RequestFilter username:"+user.getUsername());
         }
 
-        if(token.equals("123"))
+        if("123".equals(token))
             filterChain.doFilter(servletRequest, servletResponse);
         else {
             logger.debug("请求中需传入参数token=123，本过滤器才会放行。");
+            servletResponse.setCharacterEncoding("UTF-8");
+            servletResponse.getWriter().write("请求中需传入参数token=123，本拦截器才会放行。");
         }
     }
 

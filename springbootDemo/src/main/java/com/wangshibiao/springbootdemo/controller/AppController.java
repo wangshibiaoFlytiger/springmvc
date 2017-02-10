ad1074fc51975ffdd85011b4accdce4db47e445c
Wang Shibiao <645102170@qq.com>
2017-02-10 16:34:23 +0800

    优化代码：优化目录结构

--- springbootDemo/src/main/java/com/wangshibiao/springbootdemo/controller/AppController.java
+++ springbootDemo/src/main/java/com/wangshibiao/springbootdemo/controller/AppController.java
@@ -1,4 +1,4 @@
-package com.wangshibiao.springbootdemo;
+package com.wangshibiao.springbootdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

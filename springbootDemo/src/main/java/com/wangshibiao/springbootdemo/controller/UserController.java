b11c233e1e4a98a1c49e1123f58e7699822a2831
Wang Shibiao <645102170@qq.com>
2017-02-16 09:22:15 +0800

    添加功能： 1. 添加restful接口：增、删、改、查 2. 使用restTemplate调用restful接口

--- springbootDemo/src/main/java/com/wangshibiao/springbootdemo/controller/UserController.java
+++ springbootDemo/src/main/java/com/wangshibiao/springbootdemo/controller/UserController.java
@@ -3,10 +3,14 @@ package com.wangshibiao.springbootdemo.controller;
 import com.wangshibiao.springbootdemo.dao.UserDao;
 import com.wangshibiao.springbootdemo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by Wang Shibiao on 2017/2/8.
@@ -21,10 +25,36 @@ public class UserController {
         return "Hello World!";
     }
 
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ResponseEntity<User> addUser(HttpServletRequest request, HttpServletResponse response,
+                                        @RequestBody User user){
+        user = userDao.save(user);
+        return new ResponseEntity<User>(user, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser(HttpServletRequest request, HttpServletResponse response,
+                                        @RequestBody User user){
+        user = userDao.save(user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
+    public void deleteUser(HttpServletRequest request, HttpServletResponse response,
+                                        @PathVariable String id){
+        userDao.delete(id);
+    }
+
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public User findUserById(HttpServletRequest request, HttpServletResponse response,
                                  @PathVariable String id){
         User user = userDao.findById(id);
         return user;
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> findAllUsers(HttpServletRequest request, HttpServletResponse response){
+        List<User> userList = userDao.findAll();
+        return userList;
     }
 }
---
+++ springbootDemo/src/test/java/com/wangshibiao/springbootdemo/controller/UserControllerTest.java
@@ -0,0 +1,96 @@
+package com.wangshibiao.springbootdemo.controller;
+
+import com.wangshibiao.springbootdemo.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Wang Shibiao on 2017/2/14.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void testHome() throws Exception {
+
+    }
+
+    /**
+     * 测试restful api: 添加某个用户/users
+     * @throws Exception
+     */
+    @Test
+    public void testAddUser() throws Exception {
+        User user = new User();
+        user.setName("lisi");
+        user.setAge(44);
+
+        String url = "http://localhost:8080/users";
+        User userRet = restTemplate.postForObject(url, user, User.class);
+    }
+
+    /**
+     * 测试restful api: 更新某个用户/users
+     * @throws Exception
+     */
+    @Test
+    public void testUpdateUser() throws Exception {
+        User user = new User();
+        user.setId("a9c359c5-f31b-11e6-a188-00ff3b946d39");
+        user.setName("wangli_updated");
+        user.setAge(44);
+
+        String url = "http://localhost:8080/users";
+        restTemplate.put(url, user, new Object());
+    }
+
+    /**
+     * 测试restful api: 删除某个用户/users/{id}
+     * @throws Exception
+     */
+    @Test
+    public void testDeleteUser() throws Exception {
+        String url = "http://localhost:8080/users/{id}";
+        restTemplate.delete(url, "a9c359c5-f31b-11e6-a188-00ff3b946d39");
+    }
+
+    /**
+     * 测试restful api: 查询某个用户/users/{id}
+     * @throws Exception
+     */
+    @Test
+    public void testFindUserById() throws Exception {
+        //法1
+        User user = restTemplate.getForObject("http://localhost:8080/users/ade9759c-f34c-11e6-a188-00ff3b946d39", User.class);
+        //法2
+        user = restTemplate.getForObject("http://localhost:8080/users/{id}", User.class, "ade9759c-f34c-11e6-a188-00ff3b946d39");
+        //法3
+        Map paras = new HashMap<>();
+        paras.put("id", "ade9759c-f34c-11e6-a188-00ff3b946d39");
+        user = restTemplate.getForObject("http://localhost:8080/users/{id}", User.class, paras);
+    }
+
+    /**
+     * 测试restful api: 查询所有用户/users
+     * @throws Exception
+     */
+    @Test
+    public void testFindAllUsers() throws Exception {
+        String url = "http://localhost:8080/users";
+        List<User> userList = restTemplate.getForObject(url, List.class);
+    }
+}

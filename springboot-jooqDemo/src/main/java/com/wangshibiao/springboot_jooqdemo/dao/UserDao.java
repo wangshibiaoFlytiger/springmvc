3e2a543c7da2afbfa0a4af6e9763b5332c8a59fe
Wang Shibiao <645102170@qq.com>
2017-03-01 10:21:09 +0800

    修复bug： 补充上次提交遗漏的文件build.gradle
    优化代码：
    调整了目录结构

--- springboot-jooqDemo/src/main/java/com/wangshibiao/springboot_jooqdemo/dao/UserDao.java
+++ springboot-jooqDemo/src/main/java/com/wangshibiao/springboot_jooqdemo/dao/UserDao.java
@@ -1,18 +1,8 @@
 package com.wangshibiao.springboot_jooqdemo.dao;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-import com.wangshibiao.springboot_jooqdemo.ConfigProperties;
-import jooqgenerated.testdb.Tables;
-import jooqgenerated.testdb.tables.pojos.User;
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.wangshibiao.springboot_jooqdemo.jooq.generated.testdb.Tables;
+import com.wangshibiao.springboot_jooqdemo.jooq.generated.testdb.tables.pojos.User;
 import org.springframework.stereotype.Repository;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.List;
 
 /**
--- springboot-jooqDemo/src/test/java/com/wangshibiao/springboot_jooqdemo/dao/UserDaoTest.java
+++ springboot-jooqDemo/src/test/java/com/wangshibiao/springboot_jooqdemo/dao/UserDaoTest.java
@@ -1,22 +1,14 @@
 package com.wangshibiao.springboot_jooqdemo.dao;
 
 import com.wangshibiao.springboot_jooqdemo.ConfigProperties;
-import jooqgenerated.testdb.Tables;
-import jooqgenerated.testdb.tables.pojos.User;
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
+import com.wangshibiao.springboot_jooqdemo.jooq.generated.testdb.tables.pojos.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.util.List;
 
 import static org.junit.Assert.*;
